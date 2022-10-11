11e60b56685111bac18a044b45a89421db6213a8
Ariane Beltrão <arianebeltrao@hotmail.com>
2022-10-11 20:54:14 -0300

    atualizando service validacoes

--- App2/Service/ClientService.cs
+++ App2/Service/ClientService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,28 +9,35 @@ namespace App2.Service
 {
     public class ClientService
     {
-     //   public bool isNameValid(string clientName)
-     //   {
+        public string upperName(string firstName)
+        {
+            return char.ToUpper(firstName[0]) + firstName.Substring(1);
             
-     //   }
+        }
 
         public bool isCpfValid(long clientCpf)
         {
-            if (clientCpf < 11)
+            var digitQty = clientCpf.ToString().Length;
+            if (digitQty != 11)
             {
-                return "CPF inválido";
-            } else if (clientCpf > 11) 
-            {
-                return "CPF inválido";
+                return false;
+            }
+            return true;
+        
+        
 
-            } else (clientCpf == 11);
-                    return;
-                    
         }
 
-        //public bool isAgeValid(int clientAge)
-      //  {
 
-      //  }
+
+        public bool isAgeValid(int clientAge)
+        {
+            if (clientAge > 100)
+            {
+                return false;
+            } 
+            return true;
+         } 
+           
     }
 }
